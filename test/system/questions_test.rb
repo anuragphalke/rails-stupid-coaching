9b4b834628a5f8fb2a909f3c1ffb5e802cf8cdbe
Anurag Phalke <anurag.phalke@gmail.com>
2024-11-11 13:33:19 +0100

    Tests done

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -3,6 +3,6 @@ require "application_system_test_case"
 class QuestionsTest < ApplicationSystemTestCase
   test "visiting /ask renders the form" do
     visit ask_url
-    assert_selector "p", text: "Ask your coach anything"
+    assert_selector "label", text: "Ask the coach"
   end
 end
