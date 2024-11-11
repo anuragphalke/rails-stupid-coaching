c48e1789033eafe004bd32d7d698674d6ca843be
Anurag Phalke <anurag.phalke@gmail.com>
2024-11-11 13:57:03 +0100

    All done

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -4,5 +4,12 @@ class QuestionsTest < ApplicationSystemTestCase
   test "visiting /ask renders the form" do
     visit ask_url
     assert_selector "label", text: "Ask the coach"
+    test "saying Hello yields a grumpy response from the coach" do
+      visit ask_url
+      fill_in "question", with: "Hello"
+      click_on "Ask"
+
+      assert_text "I don't care, get dressed and go to work!"
+    end
   end
 end
